368328445e2559b6227f29a27fda21b088f04c25
Maaz Ali <moaaxali@gmail.com>
2021-03-02 15:57:59 +0500

    Add an express route for testing

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Automated Node.js deployments with Azure Pipelines");
+});
+
+app.get('/greet/:me', (req, res) => {
+    res.send(`Hello! ${me}`);
 });
 
 const port = process.env.PORT || 1337;
